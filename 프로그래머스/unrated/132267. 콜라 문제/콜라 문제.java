2e1e41fc242cb884d64a2691d61daa359723dc34
jangyoojeong <55098858+jangyoojeong@users.noreply.github.com>
2023-03-24 00:26:13 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,24 +1,12 @@
 class Solution {
     public int solution(int a, int b, int n) {
+        
         int result = 0;
         
-        int reCnt = 0;
-        int holdCnt = n;
-        
-        while(true)
+        while(n >= a)
         {
-            // 돌려받는 병 개수
-            reCnt = (holdCnt / a) * b;
-            
-            holdCnt = holdCnt - (holdCnt - (holdCnt % a)) + reCnt;
-
-            
-            result += reCnt;
-            
-            if (holdCnt < a) 
-            {
-                break;
-            }
+             result += (n / a) * b;
+             n = n % a + (n / a) * b;
         }
         
         return result;
