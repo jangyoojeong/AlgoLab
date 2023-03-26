40ff7c057c2d440219003b91dd8be378597667ce
jangyoojeong <55098858+jangyoojeong@users.noreply.github.com>
2023-03-26 17:06:35 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 17.21 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -8,46 +8,28 @@ class Solution {
         
         HashMap<Character,Integer> map = new HashMap<Character,Integer>();
         
-        for (int i = 0; i < arr.length; i ++) 
+        for (char[] t : arr) 
         {
-            for (int j = 0; j < arr[i].length; j ++) 
-            {
-                map.put(arr[i][j], 0);
-            }
+            map.put(t[0], 0);
+            map.put(t[1], 0);
         }
         
         for (int k = 0; k < survey.length; k ++) 
         {
-            
             // 1 ~ 3
             if (choices[k] - 4 < 0) 
             {
-                int val = map.get(survey[k].charAt(0));
-                
-                map.put(survey[k].charAt(0), val + Math.abs(choices[k] - 4));
-                
-                // System.out.println("Key : " + survey[k].charAt(0) + ", Value : " + Math.abs(choices[k] - 4));
-            }
-            else if (choices[k] - 4 > 0) 
-            {
-                int val = map.get(survey[k].charAt(1));
-                
-                map.put(survey[k].charAt(1), val + Math.abs(choices[k] - 4));
-                
-                // System.out.println("Key : " + survey[k].charAt(1) + ", Value : " + Math.abs(choices[k] - 4));
-            }
-        }
-        
-        for (int i = 0; i < arr.length; i ++) 
-        {
-            if (map.get(arr[i][0]) >= (map.get(arr[i][1])))
-            {
-                result += arr[i][0];
+                map.put(survey[k].charAt(0), map.get(survey[k].charAt(0)) + Math.abs(choices[k] - 4));
             }
             else
             {
-                result += arr[i][1];
+                map.put(survey[k].charAt(1), map.get(survey[k].charAt(1)) + Math.abs(choices[k] - 4));
             }
+        }
+        
+        for (char[] t : arr) 
+        {
+            result += map.get(t[0]) >= map.get(t[1]) ? t[0] : t[1];
         }
         
         return result;
