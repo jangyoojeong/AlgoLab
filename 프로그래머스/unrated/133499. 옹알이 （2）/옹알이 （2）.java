ff4cd2d15c4e5ee6554b429b44f499ecea0b48b0
jangyoojeong <55098858+jangyoojeong@users.noreply.github.com>
2023-03-28 23:59:56 +0900

    [unrated] Title: 옹알이 (2), Time: 0.81 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,21 +1,30 @@
 class Solution {
-    public int solution(String[] babblings) {
-        // "aya", "ye", "woo", "ma" 4가지 발음만 가능
-        int answer = 0;
-        for(int i = 0; i < babblings.length; i++) {
-            if(babblings[i].contains("ayaaya") || babblings[i].contains("yeye") || babblings[i].contains("woowoo") || babblings[i].contains("mama")) {
+    public int solution(String[] babbling) {
+        int result = 0;
+
+        for (String txt1 : babbling)
+        {
+            // 같은 발음 연속 체크
+            if(txt1.contains("ayaaya") || txt1.contains("yeye") || txt1.contains("woowoo") || txt1.contains("mama")) {
                 continue;
             }
+            
+            // "aya", "ye", "woo", "ma" 4가지 발음 가능
+            txt1 = txt1.replace("aya", " ");
+            txt1 = txt1.replace("ye", " ");
+            txt1 = txt1.replace("woo", " ");
+            txt1 = txt1.replace("ma", " ");
+            txt1 = txt1.replace(" ", "");
+            
+            
+            if (txt1.length() == 0) result++;
+            
+        }     
+        
+        
+        
 
-            babblings[i] = babblings[i].replace("aya", " ");
-            babblings[i] = babblings[i].replace("ye", " ");
-            babblings[i] = babblings[i].replace("woo", " ");
-            babblings[i] = babblings[i].replace("ma", " ");
-            babblings[i] = babblings[i].replace(" ", "");
-
-            if(babblings[i].length()  == 0) answer++;
-
-        }
-        return answer;
+        
+        return result;
     }
 }
