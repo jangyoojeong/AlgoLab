b1ed7d6ff8a5c566cdf8e469ce4b256326ae3fee
jangyoojeong <55098858+jangyoojeong@users.noreply.github.com>
2024-09-29 15:51:31 +0900

    [level 3] Title: 표 편집, Time: 886.18 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,89 +1,62 @@
 import java.util.*;
 
-class Solution {
-public String solution(int n, int k, String[] cmd) {
-        WorkNode workNode = new WorkNode(n, k);
+       class Solution {
+        public String solution(int n, int k, String[] cmd) {
+            // n : 데이터 열 개수
+            // k : 처음 선택된 인덱스
+            // cmd : 명령어 배열
 
-        for(String command: cmd) {
-            workNode.command(command);
-        }
+            // 삭제된 인덱스들은 순서대로 스택에 쌓이고, Z 명령어를 처리할 때 스택에서 삭제된 행을 복구
+            Stack<Integer> deleted = new Stack<>();
+            // 현재 길이, 인덱스 관리
+            int size = n;
+            int idx = k;
 
-        return workNode.getNowStatusBoard();
-    }
+            for (String command : cmd) {
+                String[] chArr = command.split(" ");
 
-    public static class WorkNode {
-        private int boardSize;
-        private int cursor;
-        private Stack<Integer> delHistory;
-
-        public WorkNode(int n, int k) {
-            boardSize = n;
-            cursor = k;
-            delHistory = new Stack<>();
-        }
-
-        public void command(String cmd) {
-            String[] tokens = cmd.split(" ");
-            switch (tokens[0]) {
-                case "U":
-                    int moveUp = Integer.parseInt(tokens[1]);
-                    moveUp(moveUp);
-                    break;
-                case "D":
-                    //아래로 n칸 이동
-                    int moveDown = Integer.parseInt(tokens[1]);
-                    moveDown(moveDown);
-                    break;
-                case "C":
-                    //현재행 삭제 후 아래행 선책
-                    delete();
-                    break;
-                case "Z":
-                    //가장 최근 삭제된 행을 복구 단 현재 선택된 행은 변경 X
-                    delRollback();
-                    break;
+                switch (chArr[0]) {
+                    // U X : 현재 선택된 행에서 X칸 위에 있는 행을 선택
+                    case "U":
+                        int moveUp = Integer.parseInt(chArr[1]);
+                        // 위로 이동할 때 0보다 작아지지 않도록
+                        idx = Math.max(0, idx - moveUp);
+                        break;
+                    // D X : 현재 선택된 행에서 X칸 아래에 있는 행을 선택
+                    case "D":
+                        int moveDown = Integer.parseInt(chArr[1]);
+                        // 아래로 이동할 때 남은 행의 수를 넘지 않도록
+                        idx = Math.min(size - 1, idx + moveDown);
+                        break;
+                    // C : 현재 선택된 행을 삭제한 후, 바로 아래 행을 선택 단, 삭제된 행이 가장 마지막 행인 경우 바로 윗 행을 선택
+                    case "C":
+                        // 삭제 관리 스택 적재
+                        deleted.push(idx);
+                        // 삭제되므로 사이즈 - 1
+                        size --;
+                        // 맨 마지막 행이 삭제될 경우 윗행 선택
+                        if (idx == size) {  // 삭제 후 커서가 마지막 행이면 위로 이동
+                            idx--;
+                        }
+                        break;
+                    // Z : 가장 최근에 삭제된 행을 원래대로 복구 단, 현재 선택된 행은 바뀌지 않음.
+                    case "Z":
+                        // 삭제 관리 스택에서 제거
+                        int recovered = deleted.pop();
+                        // 복귀되므로 사이즈 + 1
+                        size ++;
+                        // 복구된 행이 커서보다 앞에 있는 경우 커서 조정
+                        if(recovered <= idx) {
+                            idx++;
+                        }
+                        break;
+                }
             }
-        }
-
-        private void moveUp(int move) {
-            //위로 n칸 이동
-            if(cursor - move >= 0) {
-                cursor = cursor - move;
-            }
-        }
-
-        private void moveDown(int move) {
-            //아래로 n칸 이동
-            if(cursor + move < boardSize) {
-                cursor = cursor + move;
-            }
-        }
-
-        private void delete() {
-            //현재행 삭제 후 아래행 선책
-            delHistory.push(cursor);
-            boardSize--;
-            if(cursor == boardSize)
-                cursor--;
-        }
-
-        private void delRollback() {
-            //가장 최근 삭제된 행을 복구 단 현재 선택된 행은 변경 X
-            if(delHistory.isEmpty()) return;
-
-            int delPoint = delHistory.pop();
-            boardSize++;
-            if(delPoint <= cursor)
-                cursor++;
-        }
-
-        public String getNowStatusBoard() {
-            StringBuilder sb = new StringBuilder();
-            sb.append("O".repeat(boardSize));
-            while (!delHistory.isEmpty()) {
-                sb.insert(delHistory.pop(), "X");
+            
+            StringBuilder sb = new StringBuilder("O".repeat(size));
+            while (!deleted.isEmpty()) {
+                sb.insert(deleted.pop(), "X");
             }
             return sb.toString();
         }
     }
-}
